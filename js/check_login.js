bb74d2b896524faec193d07ee934da2324bb81c6
Dennis P Kamah <dkamah913@gmail.com>
2018-01-06 17:05:16 +0700

    add warning modal before logout

--- js/check_login.js
+++ js/check_login.js
@@ -1,22 +1,30 @@
 $(document).ready(function(){
     $("#login_link").show();
-    $("#logout_link").hide();
+    $("#logout_warning").hide();
 
     if(typeof(Storage) !== "undefined") {
         if(sessionStorage.loggedUser != undefined) {
             var pagePath = window.location.pathname;
-            var temp = pagePath.split("/"); 
+            var temp = pagePath.split("/");
             if(temp[temp.length - 1] == 'login.html') {
                 window.location.assign("index.html");
             }
             $("#login_link").hide();
-            $("#logout_link").show();
+            $("#logout_warning").show();
 
             //TODO after a user is logged on
-        } 
+        }
     } else {
         alert("Login is not available in this browser");
     }
+
+    $("#logout_warning").click(function(){
+        $("#logout_warning_modal").show();
+    });
+
+    $(".close_logout_warning_modal").click(function(){
+      $("#logout_warning_modal").hide();
+    });
 
     $("#logout_link").click(function(){
         sessionStorage.clear();
