8e99cc1e26da589882ef8a851576b796c706a0da
Dennis P Kamah <dkamah913@gmail.com>
2018-01-07 13:52:16 +0700

    fix position of works of indexpage

--- js/open_index.js
+++ js/open_index.js
@@ -22,18 +22,15 @@ $(document).ready(function(){
             var works = $(xmlDoc).find('works');
             var works_children = $(works).children();
             var works_output = "<div class=\"columns\">";
-            works_output += "<div class=\"column is-one-third\">";
-            var works_curr = works_children.first();
+            //works_output += "<div class=\"column is-one-third\">";
+            var works_curr = works_children.last();
             var i = 1;
-            while(!(works_curr.is(works_children.last()))) {
-              if(i % 3 == 0) {
-                works_output += "</div></div>";
-                works_output = "<div class=\"columns\">";
-                works_output += "<div class=\"column is-one-third\">";
-              } else if(i != 1) {
-                works_output += "<div class=\"column is-one-third\">";
+            while(!(works_curr.is(works_children.first()))) {
+              if(i != 1 && (i - 1) % 3 == 0) {
+                works_output += "</div><div class='columns'>"
               }
-              works_output += "<div class=\"card\"><div class=\"card-image\"><figure class=\"image is-4by3\">";
+              works_output += "<div class=\"column is-one-third\">";
+              works_output += "<div class=\"card\"><div class=\"card-image\"><figure class=\"image is-16by9\">";
               var works_pictures = $(works_curr).find("pictures").children();
               var works_main = $(works_pictures).first();
               works_output += "<img src='" + works_main.text() + "' alt='" + works_curr.find('name').text() + "'>";
@@ -42,11 +39,13 @@ $(document).ready(function(){
               works_output += "<p class=\"title is-4\" style=\"color:black\">" + works_curr.find('name').text() + "</p>";
               works_output += "<a>View Detail</a>";
               works_output += "</div></div></div></div>";
-              works_curr = works_curr.next();
+              works_curr = works_curr.prev();
               i++;
-              alert(works_output);
             }
-            works_curr = works_children.last();
+            works_curr = works_children.first();
+            if(i != 1 && (i - 1) % 3 == 0) {
+              works_output += "</div><div class='columns'>"
+            }
             works_output += "<div class=\"column is-one-third\">";
             works_output += "<div class=\"card\"><div class=\"card-image\"><figure class=\"image is-4by3\">";
             var works_pictures = $(works_curr).find("pictures").children();
