b317061f28edf1f3b1106dc612375bf03943e7c6
Dennis P Kamah <dkamah913@gmail.com>
2018-01-07 23:02:19 +0700

    init add modal detail for each work in index page

--- js/open_index.js
+++ js/open_index.js
@@ -29,15 +29,44 @@ $(document).ready(function(){
               if(i != 1 && (i - 1) % 3 == 0) {
                 works_output += "</div><div class='columns'>"
               }
-              works_output += "<div class=\"column is-one-third\">";
-              works_output += "<div class=\"card\"><div class=\"card-image\"><figure class=\"image is-16by9\">";
               var works_pictures = $(works_curr).find("pictures").children();
               var works_main = $(works_pictures).first();
+              works_output += "<div class=\"column is-one-third\">";
+              works_output += "<div id='detail-" + i + "' class=\"modal works_detail_modal\"><div class=\"modal-background\"></div><div class=\"modal-card\">";
+              works_output += "<header class=\"modal-card-head\"><p class=\"modal-card-title\">" + works_curr.find('name').text() + "</p><button id='close-" + i + "' class=\"delete close_works_detail\" aria-label=\"close\"></button></header>";
+              works_output += "<section class=\"modal-card-body\">";
+              works_output += "<p class=\"subtitle is-6 has-text-justified\" style=\"color:black\">" + works_curr.find('description').text() + "</p>";
+              works_output += "<aside class=\"menu\">";
+              works_output += "<p class=\"menu-label\">Role</p>";
+              works_output += "<ul class=\"menu-list\">" + "<li style=\"color:black\"><a>" + works_curr.find('role').text() + "</a></li></ul>";
+              works_output += "<p class=\"menu-label\">For</p>";
+              works_output += "<ul class=\"menu-list\">" + "<li style=\"color:black\"><a>" + works_curr.find('purpose').text() + "</a></li></ul>";
+              works_output += "<p class=\"menu-label\">Time</p>";
+              works_output += "<ul class=\"menu-list\">" + "<li style=\"color:black\"><a>" + works_curr.find('date').text() + "</a></li></ul>";
+              works_output += "<p class=\"menu-label\">Type</p>";
+              works_output += "<ul class=\"menu-list\">" + "<li style=\"color:black\"><a>" + works_curr.find('type').text() + "</a></li></ul>";
+              works_output += "</aside>";
+              works_output += "</section>";
+              works_output += "<footer class=\"modal-card-foot\">";
+              var works_details_pictures = $(works_pictures).first();
+              while(!(works_details_pictures.is(works_pictures.last()))) {
+                works_output += "<figure class=\"image is-256x256\">";
+                works_output += "<img src='" + works_details_pictures.text() + "'>";
+                works_output += "</figure>";
+                works_details_pictures = works_details_pictures.next();
+              }
+              works_details_pictures = $(works_pictures).last();
+              works_output += "<figure class=\"image is-256x256\">";
+              works_output += "<img src='" + works_details_pictures.text() + "'>";
+              works_output += "</figure>";
+              works_output += "</footer>";
+              works_output += "</div></div>";
+              works_output += "<div class=\"card\"><div class=\"card-image\"><figure class=\"image is-16by9\">";
               works_output += "<img src='" + works_main.text() + "' alt='" + works_curr.find('name').text() + "'>";
               works_output += "</figure></div>";
               works_output += "<div class=\"card-content has-text-centered\"><div class=\"content\">"
               works_output += "<p class=\"title is-4\" style=\"color:black\">" + works_curr.find('name').text() + "</p>";
-              works_output += "<a>View Detail</a>";
+              works_output += "<a id='" + i + "' class=\"view_works_detail\">View Detail</a>";
               works_output += "</div></div></div></div>";
               works_curr = works_curr.prev();
               i++;
@@ -46,17 +75,58 @@ $(document).ready(function(){
             if(i != 1 && (i - 1) % 3 == 0) {
               works_output += "</div><div class='columns'>"
             }
-            works_output += "<div class=\"column is-one-third\">";
-            works_output += "<div class=\"card\"><div class=\"card-image\"><figure class=\"image is-4by3\">";
             var works_pictures = $(works_curr).find("pictures").children();
             var works_main = $(works_pictures).first();
+            works_output += "<div class=\"column is-one-third\">";
+            works_output += "<div id='detail-" + i + "' class=\"modal\"><div class=\"modal-background\"></div><div class=\"modal-card\">";
+            works_output += "<header class=\"modal-card-head\"><p class=\"modal-card-title\">" + works_curr.find('name').text() + "</p><button id='close-" + i + "' class=\"delete close_works_detail\" aria-label=\"close\"></button></header>";
+            works_output += "<section class=\"modal-card-body\">";
+            works_output += "<p class=\"subtitle is-6 has-text-justified\" style=\"color:black\">" + works_curr.find('description').text() + "</p>";
+            works_output += "<aside class=\"menu\">";
+            works_output += "<p class=\"menu-label\">Role</p>";
+            works_output += "<ul class=\"menu-list\">" + "<li style=\"color:black\"><a>" + works_curr.find('role').text() + "</a></li></ul>";
+            works_output += "<p class=\"menu-label\">For</p>";
+            works_output += "<ul class=\"menu-list\">" + "<li style=\"color:black\"><a>" + works_curr.find('purpose').text() + "</a></li></ul>";
+            works_output += "<p class=\"menu-label\">Time</p>";
+            works_output += "<ul class=\"menu-list\">" + "<li style=\"color:black\"><a>" + works_curr.find('date').text() + "</a></li></ul>";
+            works_output += "<p class=\"menu-label\">Type</p>";
+            works_output += "<ul class=\"menu-list\">" + "<li style=\"color:black\"><a>" + works_curr.find('type').text() + "</a></li></ul>";
+            works_output += "</aside>";
+            works_output += "</section>";
+            works_output += "</section>";
+            works_output += "<footer class=\"modal-card-foot\">";
+            var works_details_pictures = $(works_pictures).first();
+            while(!(works_details_pictures.is(works_pictures.last()))) {
+              works_output += "<figure class=\"image is-256x256\">";
+              works_output += "<img src='" + works_details_pictures.text() + "'>";
+              works_output += "</figure>";
+              works_details_pictures = works_details_pictures.next();
+            }
+            works_details_pictures = $(works_pictures).last();
+            works_output += "<figure class=\"image is-256x256\">";
+            works_output += "<img src='" + works_details_pictures.text() + "'>";
+            works_output += "</figure>";
+            works_output += "</footer>";
+            works_output += "</div></div>";
+            works_output += "<div class=\"card\"><div class=\"card-image\"><figure class=\"image is-4by3\">";
             works_output += "<img src='" + works_main.text() + "' alt='" + works_curr.find('name').text() + "'></figure></div>";
             works_output += "<div class=\"card-content has-text-centered\"><div class=\"content\">"
             works_output += "<p class=\"title is-4\" style=\"color:black\">" + works_curr.find('name').text() + "</p>";
-            works_output += "<a>View Detail</a>";
+            works_output += "<a id='" + i + "' class=\"view_works_detail\">View Detail</a>";
             works_output += "</div></div></div></div>";
             works_output += "</div>";
             $("#index_works").html(works_output);
         }
+    });
+
+    $(document).on("click", ".view_works_detail", function(){
+      var id = $(this).attr('id');
+      $("#detail-" + id).show();
+    });
+
+    $(document).on("click", ".close_works_detail", function(){
+      var temp = $(this).attr('id').split('-');
+      var id = temp[1];
+      $("#detail-" + id).hide();
     });
 });
